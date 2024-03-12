834b7b25e12e98d2c622f2a9590d822ba7987b18
Michael Telahun <michael.telahun@dindin.et>
2024-03-12 22:36:08 +0300

    when two inputs of 1-each are given, output is string"11"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-#[derive(Debug)]
-struct FizzBuzz {}
+#[derive(Debug, Default)]
+struct FizzBuzz {
+    inner: String,
+}
+
 impl FizzBuzz {
-    fn run(&self, input: i32) -> String {
-        format!("{input}")
+    fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    fn run(&mut self, input: i32) -> String {
+        self.inner = format!("{}{input}", self.inner);
+
+        self.inner.clone()
     }
 }
 
@@ -17,7 +28,7 @@ mod tests {
     #[test]
     fn given_fizzbuzz_when_input_is_1_then_return_string_1() {
         // Arrange
-        let fizz_buzz = FizzBuzz {};
+        let mut fizz_buzz = FizzBuzz::new();
 
         // Act
         let res = fizz_buzz.run(1);
@@ -27,6 +38,23 @@ mod tests {
             String::from("1"),
             res,
             "Input of number 1 gives output of string \"1\""
+        );
+    }
+
+    #[test]
+    fn given_fizzbuzz_with_input_1_when_second_input_is_1_then_return_string_11() {
+        // Arrange
+        let mut fizz_buzz = FizzBuzz::new();
+        let _ = fizz_buzz.run(1);
+
+        // Act
+        let res = fizz_buzz.run(1);
+
+        // Assert
+        assert_eq!(
+            String::from("11"),
+            res,
+            "Two inputs of 1-each gives output of string\"11\""
         );
     }
 }
