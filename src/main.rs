c61a32fa83072aa27ce4d3bf8e362f726e8d32ff
Michael Telahun <michael.telahun@dindin.et>
2024-03-12 22:18:34 +0300

    first test of empty input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Debug)]
+struct FizzBuzz {}
+impl FizzBuzz {
+    fn run(&self) -> String {
+        String::from("")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::FizzBuzz;
+
+    #[test]
+    fn given_fizzbuzz_when_given_empty_input_then_return_empty_output() {
+        // Arrange
+        let fizz_buzz = FizzBuzz {};
+
+        // Act
+        let res = fizz_buzz.run();
+
+        // Assert
+        assert!(res.is_empty())
+    }
 }
